ff102a6b7d0d209dd7410da1107470aad1743fa7
Tim Oram <mitmaro@gmail.com>
2016-03-08 17:54:19 -0330

    Fixed diff output for multiple line differences

--- languages/javascript.js
+++ languages/javascript.js
@@ -3,6 +3,7 @@
 module.exports = {
 	getRunCommand: function getRunCommand(problem) {
 		var jsCommand = process.env.JS_COMMAND || 'js';
+		
 		return jsCommand + ' problems/' + problem.id + '/javascript/solution.js';
 	}
 };
